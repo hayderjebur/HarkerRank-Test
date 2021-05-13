0627d7969f2dcdbe2c5d00884088ffe46ac36df7
Hayder Jebur <hayderjabur1983@gmail.com>
2021-05-13 11:58:08 -0700

    add the logic to input field

--- src/components/customer-list/index.js
+++ src/components/customer-list/index.js
@@ -1,37 +1,49 @@
-import React,{useState}  from "react";
-import "./index.css";
+import React, { useState } from 'react';
+import './index.css';
 
 function CustomerList() {
-  const [user,setUser]=useState({
-    name:''
-  });
-  const [users,setUsers]=useState([]);
+  const [user, setUser] = useState('');
+  const [users, setUsers] = useState([]);
 
-  const onChange=(e)=>{
-    setUser({
-        [e.target.name]: e.target.value,
-      });
-     
-  }
-  const onSubmit=()=>{
-    e.preventDefault()
-   setUsers([...users,user])
-
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!user) return;
+    setUsers([...users, user]);
+    console.log(users);
+  };
   return (
-    <div className="mt-75 layout-column justify-content-center align-items-center">
-      <section onSubmit={onSubmit} className="layout-row align-items-center justify-content-center">
-        <input onChange={(onChange} value={user.name} name='name' type="text" className="large" placeholder="Name" data-testid="app-input"/>
-        <button type="submit" className="ml-30" data-testid="submit-button">Add Customer</button>
+    <div className='mt-75 layout-column justify-content-center align-items-center'>
+      <section className='layout-row align-items-center justify-content-center'>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            name='name'
+            type='text'
+            className='large'
+            placeholder='Name'
+            data-testid='app-input'
+          />
+          <button type='submit' className='ml-30' data-testid='submit-button'>
+            Add Customer
+          </button>
+
+          {users.map((user, i) => (
+            <ul className='styled mt-50' data-testid='customer-list'>
+              <li
+                key={i}
+                className='slide-up-fade-in'
+                data-testid='list-item1'
+                key='list-item1'
+              >
+                {user}
+              </li>
+            </ul>
+          ))}
+        </form>
       </section>
-      {users.map(user=>{
-      <ul className="styled mt-50" data-testid="customer-list">
-          <li className="slide-up-fade-in" data-testid="list-item1" key="list-item1" >{user.name}</li>
-      </ul>
-  })}
-    
     </div>
   );
 }
 
-export default CustomerList
+export default CustomerList;
