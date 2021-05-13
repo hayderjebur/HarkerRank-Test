e537fa3b511546be211e93fe72cc309daf7b7154
User <user.push@hackerrank.workspace>
2021-05-13 18:37:19 +0000

    [Admin Commit] save changes
    Commit by HackerRank

--- src/components/customer-list/index.js
+++ src/components/customer-list/index.js
@@ -2,30 +2,31 @@ import React,{useState}  from "react";
 import "./index.css";
 
 function CustomerList() {
-  const [name,setName]=useState({});
+  const [user,setUser]=useState({
+    name:''
+  });
   const [users,setUsers]=useState([]);
 
   const onChange=(e)=>{
-    setName({
-        ...name,
+    setUsers({
         [e.target.name]: e.target.value,
       });
      
   }
   const onSubmit=()=>{
     e.preventDefault()
-   setUsers([...users,name])
+   setUsers([...users,user])
 
   }
   return (
     <div className="mt-75 layout-column justify-content-center align-items-center">
       <section onSubmit={onSubmit} className="layout-row align-items-center justify-content-center">
-        <input onChange={(e)=>onChange(e)} value={name} type="text" className="large" placeholder="Name" data-testid="app-input"/>
+        <input onChange={(e)=>onChange(e)} value={user.name}name='name' type="text" className="large" placeholder="Name" data-testid="app-input"/>
         <button type="submit" className="ml-30" data-testid="submit-button">Add Customer</button>
       </section>
   {users.map(user=>{
       <ul className="styled mt-50" data-testid="customer-list">
-          <li className="slide-up-fade-in" data-testid="list-item1" key="list-item1" >{user}</li>
+          <li className="slide-up-fade-in" data-testid="list-item1" key="list-item1" >{user.name}</li>
       </ul>
   })}
     
